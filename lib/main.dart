1cb80448a5d66580f7386ea2eb0ed4116f98ea87
CreativeToby02 <akannitobi2@gmail.com>
2024-03-18 22:02:07 +0100

    main.dart modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_ui_kit/onboarding_first_screen.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:mobile_ui_kit/theme.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => const MyApp(),
-      ),
-    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,8 +15,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mobile UI Kit',
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       theme: MobileUITheme,
       home: const OnboardingFirstScreen(),
